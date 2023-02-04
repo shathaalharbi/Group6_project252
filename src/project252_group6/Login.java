527851d69a11099367abfa9c9969c7d8b6ba5678
shatha_alharbi <shatha3456alharbi@gmail.com>
2023-02-05 00:24:41 +0300

    shatha alharbi

---
+++ src/project252_group6/Login.java
@@ -0,0 +1,35 @@
+package project252_group6;
+
+
+public class Login implements Access {
+
+    private String username;
+    private String password;
+
+    public Login(String user, String password) {
+        this.username = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return username;
+    }
+
+    public void setUser(String user) {
+        this.username = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public User checkaccess() {
+        return null;
+    }
+
+}
