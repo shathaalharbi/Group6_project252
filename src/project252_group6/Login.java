aeb6c26c80812bfd7370bac1d7a9c7d4b931b98d
Reeman_Sabir <rmooonah@gmail.com>
2023-02-05 11:52:44 +0300

    Reeman commit4

--- src/project252_group6/Login.java
+++ src/project252_group6/Login.java
@@ -1,6 +1,20 @@
 package project252_group6;
 
-public class  Login implements Access {
+
+import project252_group6.Access;
+import project252_group6.User;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author reemansabir
+ */
+public class Login implements Access {
 
     private String username;
     private String password;
